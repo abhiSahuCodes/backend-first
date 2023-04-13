280802b7d72b1621606b3d2ba1026e34961320d7
Abhishek Sahu <118609648+abhi-sahu22@users.noreply.github.com>
2023-04-13 09:51:10 +0000

    database connected with env

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+import config from "./config/index.js";
+
+( async () => {
+    try {
+        // await mongoose.connect("mongodb://localhost:27017/ecomm")
+        await mongoose.connect(config.MONGODB_URL)
+        console.log("DB Connected!")
+
+        app.on('error', (err) => {
+            console.error("ERROR: ", err);
+            throw err
+        })
+
+        const onListening = () => {
+            console.log(`Listening on port ${config.PORT}`)
+        }
+
+        // app.listen(5000, onListening)
+        app.listen(config.PORT, onListening)
+
+    } catch (err) {
+        console.error("ERROR: ", err)
+        throw err
+    }
+}) ()
