ba77934ec1e1da8fe8b5ce018a77f1931e347528
Abhishek Sahu <118609648+abhi-sahu22@users.noreply.github.com>
2023-04-14 17:33:25 +0000

    Higher order function for controller--asyncHandler

---
+++ src/service/asyncHandler.js
@@ -0,0 +1,20 @@
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export default asyncHandler
+
+
+// const asyncHandler = "Abhishek";
+// const asyncHandler = () => {};
+// const asyncHandler = (func) => {};
+// const asyncHandler = (func) => () => {};
+// const asyncHandler = () => async () => {};
