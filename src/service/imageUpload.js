952d0c089cc8839814d89d75cbde728a506c696b
Abhishek Sahu <118609648+abhi-sahu22@users.noreply.github.com>
2023-04-19 17:33:19 +0000

    little code error changed

--- src/service/imageUpload.js
+++ src/service/imageUpload.js
@@ -1,18 +1,18 @@
 import s3 from "../config/s3.config.js";
 
 export const s3FileUpload = async ({bucketName, key, body, contentType}) => {
-    return await s3.upload: ({
+    return await s3.upload({
         Bucket: bucketName,
         Key: key,
         Body: body,
-        ContentType: ContentType
+        ContentType: contentType
     })
     .promise()
 }
 
 //To delete
 export const s3FileDelete = async ({bucketName, key}) => {
-    return await s3.deleteObject: ({
+    return await s3.deleteObject({
         Bucket: bucketName,
         Key: key
     })
